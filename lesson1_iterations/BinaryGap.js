c54c1220d5835165a4d116b272bfe642f3458dd0
Talgat S <talgat.s@protonmail.com>
2019-12-07 03:00:39 -0800

    remove console BinaryGap.js

--- lesson1_iterations/BinaryGap.js
+++ lesson1_iterations/BinaryGap.js
@@ -1,19 +1,18 @@
 function solution(N) {
-	let max = 0;
-	let counter = 0;
-	const binary = N.toString(2);
-	for (let i = 0; i < binary.length; i++) {
-		if (binary[i] === "0") {
-			counter++;
-		} else {
-			if (counter > max) {
-				max = counter;
-			}
-			counter = 0;
-		}
-	}
-	console.log(N, max);
-	return max;
+    let max = 0;
+    let counter = 0;
+    const binary = N.toString(2);
+    for (let i = 0; i < binary.length; i++) {
+        if (binary[i] === "0") {
+            counter++;
+        } else {
+            if (counter > max) {
+                max = counter;
+            }
+            counter = 0;
+        }
+    }
+    return max;
 }
 
 module.exports = solution;
